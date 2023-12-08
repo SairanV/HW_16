fa0d0cf40834ad2c2258be335f0680258687e1f7
aidyn <aidynmaskutov2004@gmail.com>
2023-12-08 18:42:04 +0600

    Добавьте файлы проекта.

---
+++ HW_16/FileManager.cs
@@ -0,0 +1,258 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW_16
+{
+    public class FileManager
+    {
+        private string currentDirectory;
+        private string logFilePath;
+
+        public FileManager(string initialDirectory, string logFileName)
+        {
+            currentDirectory = initialDirectory;
+            logFilePath = Path.Combine(initialDirectory, logFileName);
+
+            // Создаем лог-файл
+            if (!File.Exists(logFilePath))
+            {
+                File.Create(logFilePath).Close();
+            }
+        }
+
+        /// <summary>
+        /// Метод для записи действия в лог-файл
+        /// </summary>
+        /// <param name="action"></param>
+        private void LogAction(string action)
+        {
+            string logEntry = $"{DateTime.Now} - {action}";
+            File.AppendAllText(logFilePath, logEntry + Environment.NewLine);
+        }
+
+        /// <summary>
+        /// Метод для отображения содержимого текущей директории
+        /// </summary>
+
+        public void ShowCurrentDirectoryContents()
+        {
+            try
+            {
+                string[] files = Directory.GetFiles(currentDirectory);
+                string[] directories = Directory.GetDirectories(currentDirectory);
+
+                Console.WriteLine("Файлы:");
+                foreach (string file in files)
+                {
+                    Console.WriteLine(Path.GetFileName(file));
+                }
+
+                Console.WriteLine("\nДиректории:");
+                foreach (string directory in directories)
+                {
+                    Console.WriteLine(Path.GetFileName(directory));
+                }
+
+                LogAction("Просмотр содержимого директории");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Ошибка: {ex.Message}");
+            }
+        }
+
+        /// <summary>
+        /// Метод для создания нового файла
+        /// </summary>
+        /// <param name="fileName"></param>
+        public void CreateFile(string fileName)
+        {
+            try
+            {
+                File.Create(Path.Combine(currentDirectory, fileName)).Close();
+                Console.WriteLine($"Файл {fileName} создан успешно.");
+
+                LogAction($"Создание файла: {fileName}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Ошибка: {ex.Message}");
+            }
+        }
+
+        /// <summary>
+        /// Метод для создания новой директории
+        /// </summary>
+        /// <param name="directoryName"></param>
+        public void CreateDirectory(string directoryName)
+        {
+            try
+            {
+                Directory.CreateDirectory(Path.Combine(currentDirectory, directoryName));
+                Console.WriteLine($"Директория {directoryName} создана успешно.");
+
+                LogAction($"Создание директории: {directoryName}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Ошибка: {ex.Message}");
+            }
+        }
+
+        /// <summary>
+        /// Метод для удаления файла
+        /// </summary>
+        /// <param name="fileName"></param>
+        public void DeleteFile(string fileName)
+        {
+            try
+            {
+                string filePath = Path.Combine(currentDirectory, fileName);
+
+                if (File.Exists(filePath))
+                {
+                    File.Delete(filePath);
+                    Console.WriteLine($"Файл {fileName} удален успешно.");
+
+                    LogAction($"Удаление файла: {fileName}");
+                }
+                else
+                {
+                    Console.WriteLine("Файл не существует.");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Ошибка: {ex.Message}");
+            }
+        }
+
+        /// <summary>
+        /// Метод для удаления директории
+        /// </summary>
+        /// <param name="directoryName"></param>
+        public void DeleteDirectory(string directoryName)
+        {
+            try
+            {
+                string directoryPath = Path.Combine(currentDirectory, directoryName);
+
+                if (Directory.Exists(directoryPath))
+                {
+                    Directory.Delete(directoryPath, true);
+                    Console.WriteLine($"Директория {directoryName} удалена успешно.");
+
+                    LogAction($"Удаление директории: {directoryName}");
+                }
+                else
+                {
+                    Console.WriteLine("Директория не существует.");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Ошибка: {ex.Message}");
+            }
+        }
+
+        /// <summary>
+        /// Метод для копирования файла
+        /// </summary>
+        /// <param name="sourceFileName"></param>
+        /// <param name="destinationFileName"></param>
+        public void CopyFile(string sourceFileName, string destinationFileName)
+        {
+            try
+            {
+                string sourcePath = Path.Combine(currentDirectory, sourceFileName);
+                string destinationPath = Path.Combine(currentDirectory, destinationFileName);
+
+                File.Copy(sourcePath, destinationPath, true);
+                Console.WriteLine($"Файл {sourceFileName} скопирован в {destinationFileName}.");
+
+                LogAction($"Копирование файла: {sourceFileName} -> {destinationFileName}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Ошибка: {ex.Message}");
+            }
+        }
+
+        /// <summary>
+        /// Метод для перемещения файла
+        /// </summary>
+        /// <param name="sourceFileName"></param>
+        /// <param name="destinationFileName"></param>
+        public void MoveFile(string sourceFileName, string destinationFileName)
+        {
+            try
+            {
+                string sourcePath = Path.Combine(currentDirectory, sourceFileName);
+                string destinationPath = Path.Combine(currentDirectory, destinationFileName);
+
+                File.Move(sourcePath, destinationPath);
+                Console.WriteLine($"Файл {sourceFileName} перемещен в {destinationFileName}.");
+
+                LogAction($"Перемещение файла: {sourceFileName} -> {destinationFileName}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Ошибка: {ex.Message}");
+            }
+        }
+
+        /// <summary>
+        /// Метод для чтения из файла
+        /// </summary>
+        /// <param name="fileName"></param>
+        public void ReadFromFile(string fileName)
+        {
+            try
+            {
+                string filePath = Path.Combine(currentDirectory, fileName);
+
+                if (File.Exists(filePath))
+                {
+                    string content = File.ReadAllText(filePath);
+                    Console.WriteLine($"Содержимое файла {fileName}:\n{content}");
+
+                    LogAction($"Чтение файла: {fileName}");
+                }
+                else
+                {
+                    Console.WriteLine("Файл не существует.");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Ошибка: {ex.Message}");
+            }
+        }
+
+        /// <summary>
+        /// Метод для записи в файл
+        /// </summary>
+        /// <param name="fileName"></param>
+        /// <param name="content"></param>
+        public void WriteToFile(string fileName, string content)
+        {
+            try
+            {
+                string filePath = Path.Combine(currentDirectory, fileName);
+
+                File.WriteAllText(filePath, content);
+                Console.WriteLine($"Текст успешно записан в файл {fileName}.");
+
+                LogAction($"Запись в файл: {fileName}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Ошибка: {ex.Message}");
+            }
+        }
+    }
+}
---
+++ HW_16/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW_16
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            FileManager fileManager = new FileManager("C:\\Temp", "LogFile.txt");
+
+            while (true)
+            {
+                Console.WriteLine("Простой файловый менеджер");
+                Console.WriteLine("1. Просмотр содержимого текущей директории");
+                Console.WriteLine("2. Создание файла");
+                Console.WriteLine("3. Создание директории");
+                Console.WriteLine("4. Удаление файла");
+                Console.WriteLine("5. Удаление директории");
+                Console.WriteLine("6. Копирование файла");
+                Console.WriteLine("7. Перемещение файла");
+                Console.WriteLine("8. Чтение из файла");
+                Console.WriteLine("9. Запись в файл");
+                Console.WriteLine("0. Выход");
+
+                Console.Write("Выберите действие (введите номер): ");
+                string choice = Console.ReadLine();
+
+                switch (choice)
+                {
+                    case "1":
+                        fileManager.ShowCurrentDirectoryContents();
+                        break;
+                    case "2":
+                        Console.Write("Введите имя файла: ");
+                        string fileName = Console.ReadLine();
+                        fileManager.CreateFile(fileName);
+                        break;
+                    case "3":
+                        Console.Write("Введите имя директории: ");
+                        string directoryName = Console.ReadLine();
+                        fileManager.CreateDirectory(directoryName);
+                        break;
+                    case "4":
+                        Console.Write("Введите имя файла для удаления: ");
+                        fileName = Console.ReadLine();
+                        fileManager.DeleteFile(fileName);
+                        break;
+                    case "5":
+                        Console.Write("Введите имя директории для удаления: ");
+                        directoryName = Console.ReadLine();
+                        fileManager.DeleteDirectory(directoryName);
+                        break;
+                    case "6":
+                        Console.Write("Введите имя файла для копирования: ");
+                        string sourceFileName = Console.ReadLine();
+                        Console.Write("Введите имя файла для вставки: ");
+                        string destinationFileName = Console.ReadLine();
+                        fileManager.CopyFile(sourceFileName, destinationFileName);
+                        break;
+                    case "7":
+                        Console.Write("Введите имя файла для перемещения: ");
+                        sourceFileName = Console.ReadLine();
+                        Console.Write("Введите имя файла для вставки: ");
+                        destinationFileName = Console.ReadLine();
+                        fileManager.MoveFile(sourceFileName, destinationFileName);
+                        break;
+                    case "8":
+                        Console.Write("Введите имя файла для чтения: ");
+                        fileName = Console.ReadLine();
+                        fileManager.ReadFromFile(fileName);
+                        break;
+                    case "9":
+                        Console.Write("Введите имя файла для записи: ");
+                        fileName = Console.ReadLine();
+                        Console.Write("Введите текст для записи в файл: ");
+                        string content = Console.ReadLine();
+                        fileManager.WriteToFile(fileName, content);
+                        break;
+                    case "0":
+                        return;
+                    default:
+                        Console.WriteLine("Некорректный ввод. Пожалуйста, повторите.");
+                        break;
+                }
+
+                Console.WriteLine("\nНажмите Enter для продолжения...");
+                Console.ReadLine();
+                Console.Clear();
+            }
+        }
+    }
+}
